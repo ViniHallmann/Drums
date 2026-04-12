16126eb5e9ec73fde345d91f940448d1a63f192d
ViniHallmann <Viniciushallmann2015@gmail.com>
2026-04-12 17:20:18 -0300

    FIx midi input

--- V1/src/core/MIDIEngine.ts
+++ V1/src/core/MIDIEngine.ts
@@ -5,10 +5,19 @@ export class MIDIEngine {
 
     async initialize(): Promise<void> {
         try {
-        this.midiAccess = await navigator.requestMIDIAccess();
-        console.log('MIDI Access granted');
+            this.midiAccess = await navigator.requestMIDIAccess();
+            console.log('MIDI Access granted');
+            //ARRUMAR ISSO AQUI PRA SER UMA INTERFACE DE DISPOSITIVO MIDI, PRA PODER SELECIONAR O DISPOSITIVO
+            //GAMBIARRA PRA SELECIONAR O PRIMEIRO DISPOSITIVO MIDI ENCONTRADO
+            const inputs = Array.from(this.midiAccess.inputs.values());
+            if (inputs.length > 0) {
+                //ISSO SO VAI FUNCIONAR NA MINHA MAQUINA, VER OUTRA MANEIA DE SELECIONAR O DISPOSITIVO CORRETO
+                this.selectDevice(inputs[1].id);
+            } else {
+                console.warn('No MIDI devices found');
+            }
         } catch (error) {
-        throw new Error('Failed to get MIDI access: ' + error);
+            throw new Error('Failed to get MIDI access: ' + error);
         }
     }
 
@@ -39,13 +48,14 @@ export class MIDIEngine {
     }
 
     private handleMIDIMessage(event: MIDIMessageEvent): void {
+        console.log('MIDI Message received:', event.data);
         const data = event.data;
         if (!data) return;
         const [status, note, velocity] = data;
         
         // Note On (144-159) com velocity > 0
         if (status >= 144 && status <= 159 && velocity > 0) {
-        this.onNoteCallback?.(note, velocity);
+            this.onNoteCallback?.(note, velocity);
         }
     }
 
